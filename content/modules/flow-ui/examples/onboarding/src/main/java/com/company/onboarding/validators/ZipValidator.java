9a16698a567bae9be712fbc4514e1e0754c291fa
Konstantin Krivopustov <krivopustov@haulmont.com>
2025-08-29 17:06:55 +0400

    Update custom validator section #1028

--- content/modules/flow-ui/examples/onboarding/src/main/java/com/company/onboarding/validators/ZipValidator.java
+++ content/modules/flow-ui/examples/onboarding/src/main/java/com/company/onboarding/validators/ZipValidator.java
@@ -2,18 +2,15 @@ package com.company.onboarding.validators;
 
 import io.jmix.flowui.component.validation.Validator;
 import io.jmix.flowui.exception.ValidationException;
-import org.springframework.beans.factory.config.BeanDefinition;
-import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 // tag::ZipValidator[]
 @Component
-@Scope(BeanDefinition.SCOPE_PROTOTYPE)
 public class ZipValidator implements Validator<String> {
     @Override
     public void accept(String value) throws ValidationException {
-        if (value != null && value.length() != 6)
-            throw new ValidationException("Zip must be of 6 characters length");
+        if (value != null && value.length() != 5)
+            throw new ValidationException("Zip code must consist of 5 characters");
     }
 }
 // end::ZipValidator[]
