a0560705e838ef9dd6cf6a9b1ead9716806eee94
Gleb Gorelov <g.gorelov@haulmont.com>
2025-09-17 16:34:03 +0400

    Document UserMenu #1047 (#1049)

--- content/modules/flow-ui/examples/onboarding/frontend/src/component/theme-toggle/theme-toggle.js
+++ content/modules/flow-ui/examples/onboarding/frontend/src/component/theme-toggle/theme-toggle.js
@@ -5,7 +5,7 @@ import {defineCustomElement} from '@vaadin/component-base/src/define.js';
 import {ElementMixin} from '@vaadin/component-base/src/element-mixin.js';
 import {TooltipController} from "@vaadin/component-base/src/tooltip-controller";
 import {css, ThemableMixin} from '@vaadin/vaadin-themable-mixin/vaadin-themable-mixin.js';
-import {buttonStyles} from '@vaadin/button/src/vaadin-button-base.js';
+import {buttonStyles} from '@vaadin/button/src/vaadin-button-core-styles.js';
 import {button as buttonLumoStyles} from '@vaadin/button/theme/lumo/vaadin-button-styles.js';
 import {ButtonMixin} from '@vaadin/button/src/vaadin-button-mixin.js';
 
