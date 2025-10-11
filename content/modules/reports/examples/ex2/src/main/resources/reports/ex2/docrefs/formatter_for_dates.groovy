6960a05806055aca0d93b82e6fbdbc4a26a52121
Konstantin Krivopustov <krivopustov@haulmont.com>
2025-10-11 20:02:21 +0400

    Design-time reports (1061)

--- content/modules/reports/examples/ex2/src/main/resources/reports/ex2/docrefs/formatter_for_dates.groovy
+++
@@ -1,7 +0,0 @@
-import java.text.SimpleDateFormat
-
-def simpleOldDateFormat = new SimpleDateFormat('yyyy-MM-dd HH:mm')
-def simpleNewDateFormat = new SimpleDateFormat('dd/MM/yyyy HH:mm')
-def oldDate = simpleOldDateFormat.parse(value)
-
-return simpleNewDateFormat.format(oldDate)
