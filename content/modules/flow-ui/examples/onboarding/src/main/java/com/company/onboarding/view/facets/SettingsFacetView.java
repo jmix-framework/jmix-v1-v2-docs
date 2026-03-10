085519d2c88faee070917d0439e2dddd31696229
sukhova <sukhova@haulmont.com>
2026-03-10 14:59:55 +0400

    Document facets in Fragment #1093

--- content/modules/flow-ui/examples/onboarding/src/main/java/com/company/onboarding/view/facets/SettingsFacetView.java
+++ content/modules/flow-ui/examples/onboarding/src/main/java/com/company/onboarding/view/facets/SettingsFacetView.java
@@ -6,6 +6,8 @@ import com.company.onboarding.view.main.MainView;
 import com.vaadin.flow.router.Route;
 import io.jmix.flowui.component.checkbox.JmixCheckbox;
 import io.jmix.flowui.facet.SettingsFacet;
+import io.jmix.flowui.facet.ViewSettingsFacet;
+import io.jmix.flowui.facet.settings.ViewSettings;
 import io.jmix.flowui.view.*;
 
 import java.util.Optional;
@@ -21,23 +23,23 @@ public class SettingsFacetView extends StandardView {
     // end::checkbox[]
     // tag::settings[]
     @ViewComponent
-    private SettingsFacet settings;
+    private ViewSettingsFacet settings;
 
     // end::settings[]
 
     // tag::applySettingsDelegate[]
     @Install(to = "settings", subject = "applySettingsDelegate")
-    private void settingsApplySettingsDelegate(final SettingsFacet.SettingsContext settingsContext) {
+    private void settingsApplySettingsDelegate(final SettingsFacet.SettingsContext<ViewSettings> settingsContext) {
         settings.applySettings();
-        Optional<Boolean> value = settingsContext.getViewSettings().getBoolean("checkbox", "value");
+        Optional<Boolean> value = settingsContext.getSettings().getBoolean("checkbox", "value");
         checkbox.setValue(value.orElse(Boolean.FALSE));
     }
     // end::applySettingsDelegate[]
 
     // tag::saveSettingsDelegate[]
     @Install(to = "settings", subject = "saveSettingsDelegate")
-    private void settingsSaveSettingsDelegate(final SettingsFacet.SettingsContext settingsContext) {
-        settingsContext.getViewSettings().put("testCheckbox", "value", checkbox.getValue());
+    private void settingsSaveSettingsDelegate(final SettingsFacet.SettingsContext<ViewSettings> settingsContext) {
+        settingsContext.getSettings().put("testCheckbox", "value", checkbox.getValue());
         settings.saveSettings();
     }
     // end::saveSettingsDelegate[]
